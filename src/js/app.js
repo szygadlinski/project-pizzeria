62eac583f39cddc5e3c71e21a478f3e2990defc0
Szymon Zygadliński <szygadlinski@gmail.com>
2020-12-13 19:05:53 +0100

    Update files from GitHub

--- src/js/app.js
+++ src/js/app.js
@@ -8,7 +8,7 @@ const app = {
     console.log('thisApp.data:', thisApp.data);
 
     for (let productData in thisApp.data.products){
-      new Product(productData, thisApp.data.products[productData]);
+      new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
     }
   },
 
