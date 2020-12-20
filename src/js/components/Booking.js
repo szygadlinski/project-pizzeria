9bbc942c453c3ece13ab806444cc07f0a3c6a77b
Szymon Zygadliński <szygadlinski@gmail.com>
2020-12-20 02:42:59 +0100

    Add .js files BaseWidget, DatePicker, HourPicker && add flatpickr, rangeslider && change AmountWidget.js

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,7 @@
 import {select, templates} from '../settings.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 class Booking {
 
@@ -21,22 +23,23 @@ class Booking {
 
     thisBooking.dom.peopleAmount = element.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = element.querySelector(select.booking.hoursAmount);
+    thisBooking.dom.datePicker = element.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hourPicker = element.querySelector(select.widgets.hourPicker.wrapper);
   }
 
   initWidgets(){
     const thisBooking = this;
 
     new AmountWidget(thisBooking.dom.peopleAmount);
-
-    thisBooking.dom.peopleAmount.addEventListener('updated', function(){
-
-    });
+    thisBooking.dom.peopleAmount.addEventListener('updated', function(){});
 
     new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.dom.hoursAmount.addEventListener('updated', function(){});
 
-    thisBooking.dom.hoursAmount.addEventListener('updated', function(){
+    new DatePicker(thisBooking.dom.datePicker);
 
-    });
+    new HourPicker(thisBooking.dom.hourPicker);
+
   }
 }
 
