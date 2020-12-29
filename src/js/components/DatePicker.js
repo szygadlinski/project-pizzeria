084aed33eb229469c2fef5a549752bae577e30c5
Szymon Zygadliński <szygadlinski@gmail.com>
2020-12-29 04:20:31 +0100

    Fix bugs in importing BaseWidget.js

--- src/js/components/DatePicker.js
+++ src/js/components/DatePicker.js
@@ -1,4 +1,4 @@
-import BaseWidget from '../Components/BaseWidget.js';
+import BaseWidget from './BaseWidget.js';
 import utils from '../utils.js';
 import {select, settings} from '../settings.js';
 
--- src/js/components/HourPicker.js
+++ src/js/components/HourPicker.js
@@ -1,4 +1,4 @@
-import BaseWidget from '../Components/BaseWidget.js';
+import BaseWidget from './BaseWidget.js';
 import {select, settings} from '../settings.js';
 import utils from '../utils.js';
 
